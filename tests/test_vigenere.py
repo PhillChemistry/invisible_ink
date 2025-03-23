af36a99e7c9a8f9daadfb10f4e712cdb2276aec1
phill <phillip.rein@outlook.com>
2025-03-23 01:01:58 +0100

    encoding vigenere ciphers now passes all tests

--- tests/test_vigenere.py
+++ tests/test_vigenere.py
@@ -1,1 +1,14 @@
-import invisible_ink.helpers.vigenere
+import invisible_ink.helpers.vigenere as v
+import pytest
+
+TEST_KEY = 'bread'
+TEST_MSG = 'he was really craving gummy bears'
+
+@pytest.fixture
+def create_vigenere():
+    '''create Vigenere object'''
+    return v.VigenereEncoder(TEST_KEY, TEST_MSG)
+
+
+def test_vigenere_init(create_vigenere):
+    assert isinstance(create_vigenere, v.VigenereEncoder)
